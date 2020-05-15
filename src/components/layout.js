db2eb8e3cee724a5e9ff2b390da6f5bf24cd866f
Mehmet Celik <git@mehmetcelik.com>
2020-05-16 01:49:26 +0300

    Package updates.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ( props ) => {
   return (
-    <div id = 'site' className = 'flex flex-col min-h-screen relative px-4'>
+    <div id = 'site' className = 'flex flex-col relative px-4'>
       <main className = 'relative flex flex-auto content-center flex-wrap -mx-4 p-4' >
         { props.children }
       </main>

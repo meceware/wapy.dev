e527b6033b9214b82af730c69afe97e4c7fad5a8
meceware <16402717+meceware@users.noreply.github.com>
2025-01-04 19:05:04 +0200

    Header scroll animation is added.

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,32 @@
 'use client';
 
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useEventListener, useDebounceCallback } from 'usehooks-ts';
+import { cn } from '@/lib/utils';
 import { ModeToggle } from '@/components/mode-toggle';
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
 
 export default function Header({ mainNavigation = (<></>), iconNavigation = (<></>) }) {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const debounced = useDebounceCallback(() => {
+    const currentScrollPos = window.scrollY;
+    const isScrollingDown = prevScrollPos < currentScrollPos;
+
+    setVisible(!isScrollingDown || currentScrollPos < 80);
+    setPrevScrollPos(currentScrollPos);
+  }, 100)
+
+  useEventListener('scroll', debounced);
+
   return (
-    <header className='border-b w-full'>
+    <header className={ cn(
+      'sticky top-0 w-full shadow border-b bg-background transition-transform duration-300',
+      { 'transform': visible, '-translate-y-full': !visible }
+    ) }>
       <div className='container mx-auto px-4'>
         <div className='h-16 flex items-center justify-between'>
           {/* Logo and Brand */}

ab9b901db27f0449ff5c96c2787b493c558e11e3
meceware <16402717+meceware@users.noreply.github.com>
2025-02-13 23:38:18 +0200

    Filter menu is converted to dialog and currency filter is added. (#47)

--- src/components/ui/scroll-area.jsx
+++ src/components/ui/scroll-area.jsx
@@ -5,12 +5,12 @@ import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 
 import { cn } from "@/lib/utils"
 
-const ScrollArea = React.forwardRef(({ className, children, ...props }, ref) => (
+const ScrollArea = React.forwardRef(({ className, viewportClassName, children, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
     {...props}>
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+    <ScrollAreaPrimitive.Viewport className={cn("h-full w-full rounded-[inherit]", viewportClassName)}>
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
